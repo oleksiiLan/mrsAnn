cdeee5f91245e9b9f11aac25140e6d3b38ded023
“o_lan” <lant91a@gmail.coml>
2023-07-12 01:27:00 +0200

    seted up environment

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+module.exports = ({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  watchForFileChanges: false,
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
